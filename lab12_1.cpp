4c3dd9339e15e57a636b9136793d47cb2a61c060
TANGCPE64 <natsuphat_th@cmu.ac.th>
2021-12-21 14:25:30 +0700

    Completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future." << "\n";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    cout << "You will get ";
+    if( x == 0 ) cout << "A";
+    else if( x == 1 ) cout << "B+";
+    else if( x == 2 ) cout << "B";
+    else if( x == 3 ) cout << "C+";
+    else if( x == 4 ) cout << "C";
+    else if( x == 5 ) cout << "D+";
+    else if( x == 6 ) cout << "D";
+    else if( x == 7 ) cout << "F";
+    else if( x == 8 ) cout << "W";
+    cout << " in this 261102.";
+    return 0;
+}
